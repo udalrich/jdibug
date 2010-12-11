2ca50e740dd9a558d0cd5b813ddde20cf63b7609
udalrich.schermer <udalrich.schermer@05857f3e-3b4d-0410-831f-07aa67c712d4>
2010-12-11 02:21:25 +0000

    Implement breakpoints when an exception occurs

--- test/java/src/com/jdibug/Main.java
+++ test/java/src/com/jdibug/Main.java
@@ -1,12 +1,15 @@
 package com.jdibug; // Generated package name
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.ArrayList;
 
 public class Main
 {
@@ -38,7 +41,24 @@ public class Main
 
         List<String> list = Arrays.asList("foo", "bar");
 
+		// Large array for testing display of sub-arrays
+		Object[] largeArray = new Object[20000];
+		largeArray[17] = floatArray;
+		largeArray[173] = list;
+
+		largeArray = new Object[23456];
+		largeArray[1732] = floatArray;
+		largeArray[8542] = list;
+
         System.out.println(list);
+
+		Map<Integer, Number> numberMap = new HashMap<Integer, Number>();
+		Collection<String> bigCollection = new TreeSet<String>();
+		for (int number = 0; number < 52; ++number)
+		{
+			numberMap.put(number, 2.3*number);
+			bigCollection.add("entry " + number);
+		}
 
         Main main = new Main();
         main.submitJobs();
--- test/smoke-tests.el
+++ test/smoke-tests.el
@@ -7,4 +7,25 @@
 (load "uninterrupt.el")
 (load "parsing.el")
 
-(elunit "smoke-test-suite")
+(defvar jdibug-smoke-failure-count)
+(defun jdibug-smoke-done-running (pass fail)
+  (setq jdibug-smoke-failure-count (+ jdibug-smoke-failure-count fail)))
+
+
+(let ((jdibug-smoke-failure-count 0))
+  (elunit "smoke-test-suite")
+  (if (and (> jdibug-smoke-failure-count 0)
+		   (if (featurep 'xemacs)
+			   (noninteractive)
+			 noninteractive))
+	  ;; Running in batch mode and we had failures.  Exit with an error
+	  (progn
+		(message "%s" (save-excursion
+						(set-buffer "*elunit report*")
+						(buffer-string)))
+		;; It seems that the exit code here is ignored.  So the build
+		;; will succeed even when thre are errors.
+		(kill-emacs jdibug-smoke-failure-count))))
+
+
+
